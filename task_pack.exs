820d9cdb2e689c856a39e98cf43443c0538c4076
Vitaly <vtmilyakov@gmail.com>
2026-01-22 11:54:37 +0100

    Add more tasks

--- task_pack.exs
+++ task_pack.exs
@@ -1,16 +1,19 @@
 defmodule TaskPackCreator do
   import Ecto.Query
 
-
-  @names  [
-  "double_letters",
-  "compress_repeats",
-  "sum_numbers_in_text",
-  "uncompress_repeats",
-  "typed_string_equality",
-  "bike_to_charge",
-  "overlapping_intervals",
-  "log_ip_sanitizer",
+  @names [
+    "clue_intersection",
+    "squad_split",
+    "secret_lab_shift_hours",
+    "eggo_boxes_needed",
+    "walkie_talkie_range",
+    "christmas_lights_groups",
+    "demogorgon_alert",
+    "portal_sightings_sum",
+    "hawkins_missing",
+    "upside_down_label",
+    "episode_binge_min",
+    "lab_patrol_coverage"
   ]
 
   def run do
@@ -20,7 +23,7 @@ defmodule TaskPackCreator do
     ids = Enum.map(@names, fn name -> tasks_by_name[name].id end)
 
     task_pack = %Codebattle.TaskPack{
-      name: "masters_s0_2025_2",
+      name: "masters_s0_2026_1",
       state: "active",
       visibility: "hidden",
       task_ids: ids,
@@ -29,6 +32,6 @@ defmodule TaskPackCreator do
 
     Codebattle.Repo.insert(task_pack)
   end
-
 end
+
 TaskPackCreator.run()
